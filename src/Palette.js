b1ce26de293fe5b7bb7c1908d49a095006667908
Gurveer Singh <gurveer.singh1608@gmail.com>
2020-08-05 01:58:29 -0400

    created initial palette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+	render() {
+		return (
+			<div className="Palette">
+				{/*Navbar Goes here */}
+				<div className="Palette-Colors">{/* bunch of color boxed */}</div>
+				{/* footer eventually */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
