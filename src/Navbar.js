d332976e77813b00e14890274a7c3b438ea83b01
Gurveer Singh <gurveer.singh1608@gmail.com>
2020-08-08 19:07:49 -0400

    added navbar and footer to single color palette

--- src/Navbar.js
+++ src/Navbar.js
@@ -34,36 +34,38 @@ class Navbar extends Component {
 		this.props.handleChange(e.target.value);
 	}
 	render() {
-		const { level, changeLevel } = this.props;
+		const { level, changeLevel, showingAllColors } = this.props;
 		const { format } = this.state;
 		return (
 			<header className="Navbar">
 				<div className="logo">
 					<Link to="/">React-Colors</Link>
 				</div>
-				<div className="slider-container">
-					<span>Level: {level}</span>
-					<div className="slider">
-						<Slider
-							defaultValue={level}
-							min={100}
-							max={900}
-							step={100}
-							onAfterChange={changeLevel}
-							trackStyle={{ backgroundColor: 'transparent' }}
-							handleStyle={{
-								backgroundColor: 'green',
-								outline: 'none',
-								border: '2px solid green',
-								boxShadow: 'none',
-								width: '13px',
-								height: '13px',
-								margin: '-3px -7px 0 0',
-							}}
-							railStyle={{ height: '8px' }}
-						/>
+				{showingAllColors && (
+					<div className="slider-container">
+						<span>Level: {level}</span>
+						<div className="slider">
+							<Slider
+								defaultValue={level}
+								min={100}
+								max={900}
+								step={100}
+								onAfterChange={changeLevel}
+								trackStyle={{ backgroundColor: 'transparent' }}
+								handleStyle={{
+									backgroundColor: 'green',
+									outline: 'none',
+									border: '2px solid green',
+									boxShadow: 'none',
+									width: '13px',
+									height: '13px',
+									margin: '-3px -7px 0 0',
+								}}
+								railStyle={{ height: '8px' }}
+							/>
+						</div>
 					</div>
-				</div>
+				)}
 				<div className="select-container">
 					<Select onChange={this.handleChange} value={format}>
 						<MenuItem value="hex">HEX</MenuItem>
