1da5dd3afa2eb905fecf3a1b6812a1306f96c583
Gurveer Singh <gurveer.singh1608@gmail.com>
2020-08-09 11:04:43 -0400

    moved styles to own seperate folder

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+	footer: {
+		height: '5vh',
+		backgroundColor: 'white',
+		display: 'flex',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		fontWeight: 'bold',
+	},
+	emoji: {
+		fontSize: '1.5rem',
+		margin: '1rem',
+	},
+};
